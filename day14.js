2051cbee702a71c0a4c61a0b58b21108e63fb0bd
Seph Kramer <seph@bounceexchange.com>
2016-01-07 16:52:31 -0500

    finish day 14

--- day14.js
+++ day14.js
@@ -34,3 +34,59 @@ for (var i = 0; i < lines.length; i++) {
 }
 
 
+function Reindeer(km, fly, rest){
+    this.km = km;
+    this.fly = fly;
+    this.rest = rest;
+    this.totalPoints = 0;
+}
+
+Reindeer.prototype.totalDistance = function(time) {
+    var restPlusFly = this.fly + this.rest;
+    var numFullCycles = Math.floor(time/restPlusFly);
+    var distancePerCycle = this.fly * this.km;
+    var remainder = time % restPlusFly;
+    var totalDistance = numFullCycles * distancePerCycle;
+    if (remainder > this.fly){
+        totalDistance += this.fly * this.km;
+    }else{
+        totalDistance += remainder * this.km;
+    }
+    return totalDistance;
+};
+
+
+
+var reindeers = {};
+
+var lines = document.body.textContent.split('\n');
+
+// var lines = ['Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.','Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.'];
+
+for (var i = 0; i < lines.length; i++) {
+    var matches = lines[i].match(/(\w+) can fly (\d+) km\/s for (\d+) seconds, but then must rest for (\d+) seconds./);
+    if (matches){
+        console.log(matches[1]);
+        reindeers[matches[1]] = new Reindeer(parseInt(matches[2]), parseInt(matches[3]), parseInt(matches[4]));
+    }
+}
+
+function findLeadReindeer(time, reindeers){
+    var maxDist = 0;
+    var leadReindeer = null;
+    for (key in reindeers){
+        // debugger;
+        var reindeerDist = reindeers[key].totalDistance(time);
+        if (reindeerDist > maxDist){
+            maxDist = reindeerDist;
+            leadReindeer = key;
+        }
+    }
+    return leadReindeer;
+}
+
+for (var i = 1; i <= 2503; i++) {
+    var leadReindeerName = findLeadReindeer(i, reindeers);
+    reindeers[leadReindeerName].totalPoints += 1;
+}
+
